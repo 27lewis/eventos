13c90f43012272de618a9302df0735fec607e924
lewis escorcia <106995911+27lewis@users.noreply.github.com>
2025-08-10 02:25:43 -0500

    DB eventos

---
+++ registro-autores.sql
@@ -0,0 +1,12 @@
+INSERT INTO `autores` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`,`salario`)
+VALUES
+  (1,"Whilemina","Hancock","283-4163 Praesent Ave","+573777439669","nec.tempus@gmail.com","44273"),
+  (2,"Mark","Robertson","5183 At Ave","+573225734641","eu@gmail.com","40216"),
+  (3,"Ishmael","Byers","127-1636 Eget Rd.","+573327872529","sit.amet.metus@outlook.edu","34353"),
+  (4,"Alice","Gillespie","Ap #259-8611 Mus. Avenue","+573853194876","dolor.egestas@gmail.com","44189"),
+  (5,"Luke","Tillman","327-8996 Velit Avenue","+573567382435","sodales@outlook.com","88538"),
+  (6,"Odysseus","Elliott","9891 Nullam St.","+573621873125","duis@gmail.com","83115"),
+  (7,"Iona","Cardenas","7564 Lacus. St.","+573646583443","tincidunt.dui@outlook.edu","98321"),
+  (8,"Daquan","Hardy","Ap #504-6457 Elementum Avenue","+573852245214","id.libero.donec@gmail.com","54592"),
+  (9,"Edan","Mendez","P.O. Box 136, 2009 Adipiscing St.","+573167522484","donec.nibh@gmail.com","16605"),
+  (10,"Georgia","Cash","P.O. Box 535, 1935 Dui, St.","+573754892232","mauris.ut@gmail.com","16750");
---
+++ registro-clientes.sql
@@ -0,0 +1,144 @@
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (1,"Otto","Hart","Ap #868-3648 Vel, Street","+573745562653","urna.vivamus.molestie@outlook.com"),
+  (2,"Carl","Gutierrez","Ap #310-1292 Pharetra, St.","+573862572739","fringilla@outlook.com"),
+  (3,"Phelan","Villarreal","P.O. Box 910, 6686 Placerat Avenue","+573675357646","elit@outlook.com"),
+  (4,"Devin","Stokes","240-4887 Rhoncus. St.","+573424342611","nec.luctus@google.edu"),
+  (5,"Charde","Leach","P.O. Box 552, 103 Tempor Road","+573988251361","amet.lorem@outlook.com"),
+  (6,"Willa","Clayton","P.O. Box 937, 5656 Eu St.","+573787663453","et.risus.quisque@outlook.edu"),
+  (7,"Amaya","Head","Ap #628-2225 Ipsum Rd.","+573782546168","a.tortor@outlook.com"),
+  (8,"Ralph","Bender","Ap #943-6810 Ultricies Rd.","+573114323563","venenatis.a.magna@outlook.com"),
+  (9,"Dane","Buchanan","501-8962 Dignissim. St.","+573893224536","duis@google.com"),
+  (10,"Adara","Burnett","1962 Felis Av.","+573445488613","ornare.facilisis.eget@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (11,"Zephr","Coleman","888-2819 Tristique Road","+573827831562","non.dui@outlook.com"),
+  (12,"Cadman","Noble","6142 In Avenue","+573211794796","ultrices@outlook.com"),
+  (13,"Jenette","Chaney","123-2160 Risus Road","+573549429675","sodales@outlook.com"),
+  (14,"Alana","Clarke","Ap #389-5215 Non Avenue","+573133189585","ornare@outlook.com"),
+  (15,"Elvis","Robles","307-1535 Tincidunt, Road","+573782353886","magna.sed@outlook.com"),
+  (16,"Bernard","Burt","244-1192 Cubilia St.","+573758627893","pede.nunc.sed@google.com"),
+  (17,"Ingrid","Daugherty","707-3379 Vel Road","+573729689634","in.lorem@outlook.com"),
+  (18,"Brooke","Perez","P.O. Box 837, 7091 Quam St.","+573617867517","morbi.accumsan@outlook.edu"),
+  (19,"Ivan","Conrad","Ap #259-3845 Id, St.","+573618382318","aliquam.auctor.velit@outlook.com"),
+  (20,"Dominic","Rowe","Ap #453-9428 Tristique Road","+573679125674","dui.fusce.diam@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (21,"Howard","Harper","646-5051 Lacus, St.","+573213355375","iaculis.enim@outlook.edu"),
+  (22,"Griffin","Holcomb","280-4682 Morbi St.","+573638755877","urna.ut.tincidunt@outlook.com"),
+  (23,"Lars","Klein","Ap #141-8941 Vel, Av.","+573964262262","eget.magna.suspendisse@google.edu"),
+  (24,"Grace","Nichols","P.O. Box 977, 3954 Sit Avenue","+573713383527","vel.nisl@google.edu"),
+  (25,"Raven","Gross","882-9336 Adipiscing St.","+573263841763","mus.proin@outlook.edu"),
+  (26,"Britanni","Barton","Ap #267-884 Eu Rd.","+573236666498","nec@outlook.com"),
+  (27,"Shea","Greene","P.O. Box 314, 1875 Venenatis Avenue","+573864549856","convallis.ligula@google.com"),
+  (28,"Kirby","Hardin","P.O. Box 133, 6022 Integer St.","+573864833838","litora.torquent@outlook.com"),
+  (29,"Jason","Thomas","2744 Vestibulum Ave","+573657764431","duis.risus.odio@outlook.edu"),
+  (30,"Price","Sargent","P.O. Box 689, 8173 Neque Road","+573434133351","egestas.blandit@outlook.com");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (31,"Raphael","Sanford","115-1039 Dolor. Av.","+573556856749","feugiat.lorem@outlook.edu"),
+  (32,"Merritt","Hunter","Ap #886-1257 Sapien, Rd.","+573947726652","purus@google.com"),
+  (33,"Jennifer","Rosa","307-7865 Egestas. Rd.","+573332223235","elit.etiam.laoreet@google.com"),
+  (34,"Aspen","Stephens","5455 Nunc Avenue","+573528272228","orci@outlook.edu"),
+  (35,"Callum","Cherry","Ap #282-6779 Neque Rd.","+573892373762","egestas.ligula@google.com"),
+  (36,"Thomas","Estes","P.O. Box 831, 8868 Faucibus Road","+573644453553","arcu@google.edu"),
+  (37,"Ivan","Miller","801-8867 Mus. Rd.","+573248288558","cum.sociis.natoque@google.edu"),
+  (38,"Celeste","Anthony","Ap #638-9693 Mauris Street","+573755698989","interdum.feugiat.sed@google.com"),
+  (39,"Indigo","Ward","605-2522 Facilisis Rd.","+573173476623","maecenas.mi@google.edu"),
+  (40,"Philip","Tillman","P.O. Box 254, 8570 Ligula. Avenue","+573172934945","cursus.luctus.ipsum@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (41,"Ferris","Davidson","Ap #503-8181 Phasellus St.","+573222664856","donec@outlook.edu"),
+  (42,"Dillon","Gregory","Ap #121-9002 Malesuada Rd.","+573628586355","lorem@google.com"),
+  (43,"Stephen","Cannon","P.O. Box 147, 912 A, Ave","+573943248856","ut.mi@outlook.com"),
+  (44,"Fredericka","Maxwell","P.O. Box 982, 9413 Malesuada Avenue","+573772952826","facilisis.magna@google.com"),
+  (45,"Carissa","Harrison","3019 Non Rd.","+573528775631","dui.nec@outlook.com"),
+  (46,"Austin","Hernandez","P.O. Box 143, 4874 Erat Avenue","+573817396128","urna.convallis@google.edu"),
+  (47,"Meghan","Roberts","360-3961 Convallis Ave","+573812229946","mauris@google.edu"),
+  (48,"Michael","Hancock","P.O. Box 455, 9257 Amet St.","+573744961156","velit.eget@outlook.com"),
+  (49,"Steel","Olsen","705-693 Etiam Av.","+573547472744","velit.aliquam@google.com"),
+  (50,"Hakeem","Santos","Ap #989-2310 Tellus. Street","+573246351886","sem.molestie@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (51,"Leigh","Hurst","490-2520 Aliquet Rd.","+573629693627","lobortis@outlook.com"),
+  (52,"Claire","Mcclure","Ap #610-9812 Tempor St.","+573768784524","tincidunt@outlook.com"),
+  (53,"Jerry","Harrington","P.O. Box 750, 443 Sed Rd.","+573287863584","lectus.quis@outlook.edu"),
+  (54,"Ryan","Lynch","652-2110 Et, St.","+573577574416","urna@google.com"),
+  (55,"Jana","Marshall","853-4112 Amet St.","+573167388656","aliquam@google.com"),
+  (56,"Silas","Austin","908-1302 Cursus Street","+573524214341","senectus.et@google.edu"),
+  (57,"Dennis","Harrison","Ap #415-7806 Et, Street","+573517192273","diam@google.com"),
+  (58,"Gwendolyn","Melendez","564-4622 Semper Rd.","+573647143832","donec@google.com"),
+  (59,"Whitney","Cross","2206 Luctus Rd.","+573671448638","mauris.elit.dictum@outlook.com"),
+  (60,"Michael","Wilson","P.O. Box 927, 8743 Mauris Rd.","+573771266826","semper.rutrum@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (61,"Slade","Avila","744-3279 Pede Av.","+573845832885","consequat.dolor@google.com"),
+  (62,"Hedley","Wilder","2884 Urna. Ave","+573395648855","nullam@google.com"),
+  (63,"Talon","Kidd","5400 Libero Street","+573358274431","laoreet.posuere@outlook.edu"),
+  (64,"Mariam","Bowman","438-1075 Dolor, Avenue","+573478813828","facilisis.facilisis.magna@outlook.edu"),
+  (65,"Chanda","Curry","Ap #913-7740 Velit. Av.","+573626768664","proin.mi@google.edu"),
+  (66,"Deborah","Cooke","P.O. Box 642, 7227 Ultricies Road","+573232129651","diam.at@outlook.edu"),
+  (67,"Remedios","Wolf","Ap #997-9672 Mauris Ave","+573457866363","enim.nec@google.edu"),
+  (68,"Vivian","Merritt","8758 Feugiat Road","+573413183378","dapibus.gravida@google.com"),
+  (69,"Tatyana","Simmons","634-3582 Convallis Avenue","+573678742386","mi@google.edu"),
+  (70,"Adrian","Barr","1718 Cras Street","+573779668265","luctus.lobortis.class@outlook.com");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (71,"Octavia","Mcguire","Ap #133-6314 Sit Rd.","+573448632363","nam.interdum@outlook.com"),
+  (72,"Kane","Charles","566-9249 Interdum Avenue","+573486237757","vestibulum.nec@outlook.com"),
+  (73,"Sybil","Aguirre","Ap #377-5202 Aliquam Rd.","+573212355568","pretium.et@outlook.edu"),
+  (74,"Mikayla","Petersen","377-6395 Lectus Ave","+573813152143","aenean@outlook.com"),
+  (75,"Cassady","Sims","238 Euismod Street","+573771828315","mi.enim@outlook.com"),
+  (76,"Neil","Nielsen","6967 Fringilla Rd.","+573737262185","pharetra@outlook.com"),
+  (77,"Sylvia","Webb","875-1381 Pretium Rd.","+573515912816","commodo@google.com"),
+  (78,"Erich","Mays","Ap #564-9508 Dignissim Rd.","+573124185545","eu.dolor@google.edu"),
+  (79,"Lev","Farrell","Ap #152-8736 Aliquet St.","+573522415658","magna.praesent@google.edu"),
+  (80,"Scarlet","Wilson","931-1801 Tellus. Road","+573737578361","sollicitudin@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (81,"Denise","Phillips","7842 Leo. Rd.","+573928366759","morbi.tristique@outlook.com"),
+  (82,"Nadine","Turner","Ap #759-9000 Tellus Rd.","+573195447538","arcu.vestibulum@outlook.com"),
+  (83,"Paula","Joseph","P.O. Box 688, 5573 Massa. St.","+573688525238","facilisis@google.com"),
+  (84,"Ori","Rasmussen","7784 Fusce St.","+573384487172","molestie.tortor.nibh@google.com"),
+  (85,"Brady","Mccall","983-9748 Sit Street","+573692814617","consectetuer.rhoncus@outlook.edu"),
+  (86,"Lacey","Brown","425-6524 Turpis Rd.","+573556283829","suspendisse.sed@google.com"),
+  (87,"Benedict","Manning","9348 Ante. St.","+573877797753","malesuada.fames.ac@outlook.com"),
+  (88,"Burke","Michael","408-3261 A, Av.","+573775176965","nulla.dignissim@google.edu"),
+  (89,"Murphy","Kemp","239-7619 Venenatis St.","+573624925371","eget@google.com"),
+  (90,"Devin","Robertson","2390 Sagittis Avenue","+573985283728","sociis.natoque@google.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (91,"Jameson","Stewart","Ap #616-7196 Elementum, Street","+573532815668","luctus.et@outlook.edu"),
+  (92,"Wynne","Walton","303-6568 Vel St.","+573525656837","cursus.diam@google.com"),
+  (93,"Kennan","Fletcher","836-7067 Magnis Rd.","+573645727723","dolor.sit@google.edu"),
+  (94,"Dane","Avila","6575 Vel St.","+573573543748","eget.massa.suspendisse@google.com"),
+  (95,"Jamal","Blackwell","343-6178 Convallis Rd.","+573855914588","amet.lorem.semper@google.edu"),
+  (96,"Raya","Dodson","P.O. Box 775, 3615 Fringilla Av.","+573783785135","blandit.mattis.cras@outlook.edu"),
+  (97,"Isabella","Mccarty","Ap #607-7370 Non St.","+573486642231","imperdiet@outlook.com"),
+  (98,"Jameson","Thornton","4890 Magna Rd.","+573654557533","nisl.quisque@google.edu"),
+  (99,"Mark","Moses","163-955 Velit St.","+573286698957","erat.vel@outlook.com"),
+  (100,"Hadassah","Carrillo","1583 In Avenue","+573428611634","aenean.euismod@outlook.edu");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (101,"Christopher","Gray","P.O. Box 333, 1337 Phasellus Ave","+573997956258","sem@google.com"),
+  (102,"Deborah","Cain","P.O. Box 835, 1945 Mauris Road","+573137278438","massa@outlook.edu"),
+  (103,"Hayes","Wiggins","Ap #387-3741 Dapibus Street","+573883774771","lectus.rutrum@outlook.com"),
+  (104,"Jade","Williams","Ap #658-7801 Gravida Road","+573426679286","penatibus.et@google.com"),
+  (105,"Quin","Shepherd","5253 Eu, Road","+573118736668","in.lobortis@outlook.com"),
+  (106,"Mason","Finch","Ap #239-3987 Risus Av.","+573354551786","cursus.purus.nullam@google.com"),
+  (107,"Jordan","Bowen","Ap #233-7984 Mauris Street","+573124824867","duis.ac@google.com"),
+  (108,"Risa","Spears","P.O. Box 423, 5262 Odio Street","+573417636855","nulla.facilisi.sed@google.edu"),
+  (109,"Neil","Mcintosh","692-2735 Orci St.","+573851446734","aenean.massa@outlook.com"),
+  (110,"Ralph","Sims","Ap #291-5661 Non, Street","+573571347758","quam.elementum@google.com");
+INSERT INTO `Clientes` (`id`,`nombres`,`apellidos`,`direccion`,`telefono`,`correo`)
+VALUES
+  (111,"Brent","Alston","735-7913 Congue, Ave","+573221832842","vel.pede@outlook.com"),
+  (112,"Darrel","Maxwell","Ap #232-475 Quis Ave","+573168962478","velit@outlook.edu"),
+  (113,"Ila","Hardin","764-565 In Street","+573733944372","consectetuer.euismod@outlook.com"),
+  (114,"Boris","Hughes","8576 Adipiscing St.","+573762548527","proin.nisl.sem@outlook.com"),
+  (115,"Xena","Norman","Ap #971-3900 Ac, Rd.","+573298164289","lacus.pede@outlook.edu"),
+  (116,"Demetrius","Jackson","Ap #352-3445 Semper, St.","+573637232566","ullamcorper.duis.at@google.edu"),
+  (117,"Deborah","Marshall","P.O. Box 111, 6066 Ligula St.","+573355637776","quam.elementum@google.edu"),
+  (118,"Debra","Mcfarland","4562 Morbi Ave","+573785846151","magna@outlook.edu"),
+  (119,"Alika","Aguilar","P.O. Box 441, 307 Adipiscing Road","+573673633746","tempor.lorem.eget@google.edu"),
+  (120,"Rooney","Mcgowan","2439 Molestie Rd.","+573579137333","eu.eleifend@google.com");
